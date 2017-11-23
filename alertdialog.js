5a84f018ddab5e0add9d7529e04737cffc9ec5c7
Unknown <drdewikurnia@gmail.com>
2017-11-23 10:14:48 +0700

    coba js

---
+++ alertdialog.js
@@ -0,0 +1,11 @@
+            function Warn() { 
+                alert ("This is a warning massage!"); 
+                document.write ("This is a warning msg!"); 
+                            } 
+            function Warn2(){ 
+                alert ("This is another warning massage!"); 
+                document.write ("This is another warning msg!"); 
+                            } 
+        
+
+            

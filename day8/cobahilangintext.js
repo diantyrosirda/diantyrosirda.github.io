a84eabcc61fe16effc359ed946688c1a16767280
Unknown <drdewikurnia@gmail.com>
2017-11-30 10:51:17 +0700

    coba hilangin text

---
+++ day8/cobahilangintext.js
@@ -0,0 +1,5 @@
+function remove() { 
+                document.getElementById(
+                                       "p").innerHTML="";
+}
+
---
+++ day8/day8js.js
@@ -0,0 +1,4 @@
+$('button').on('click',
+function(){
+$('p').css('opacity',0);
+});
